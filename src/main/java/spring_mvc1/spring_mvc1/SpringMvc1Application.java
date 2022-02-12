8e37da2256f952f0e76b0ad1b962fc528bb224eb
JellytoDev <wjdwhdgus2222@naver.com>
2022-02-12 09:57:03 +0900

    2/12-1 mvc1 commit and push

--- src/main/java/spring_mvc1/spring_mvc1/SpringMvc1Application.java
+++ src/main/java/spring_mvc1/spring_mvc1/SpringMvc1Application.java
@@ -2,7 +2,9 @@ package spring_mvc1.spring_mvc1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class SpringMvc1Application {
 
--- src/main/java/spring_mvc1/spring_mvc1/basic/HelloServlet.java
+++ src/main/java/spring_mvc1/spring_mvc1/basic/HelloServlet.java
@@ -1,4 +1,29 @@
 package spring_mvc1.spring_mvc1.basic;
 
-public class HelloServlet {
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet",urlPatterns = "/hello")
+@Slf4j
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("HelloServlet.service");
+        System.out.println("req = " + req);
+        System.out.println("resp = " + resp);
+
+        String username = req.getParameter("username");
+        System.out.println("username = " + username);
+
+        resp.setContentType("text/plain");
+        resp.setCharacterEncoding("utf-8");
+        resp.getWriter().write("hello" + username);
+    }
 }
